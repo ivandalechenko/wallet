1016fe27abce84ac0ace4be56f478a1ef2d85b1d
Иван Далеченко <ivandalechenko@gmail.com>
2023-05-21 16:02:07 +0300

    added charts in stat.html

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,124 @@
-new Chartist.Pie('#amount_by_cat_chart', {
+new Chartist.Pie('#chart', {
     series: [20, 30, 50]
 });
+
+document.getElementById('am_by_cat_btn').onclick = function () {
+    btns = document.getElementsByClassName("stat_nav_btn");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].classList.remove('active');
+    }
+    document.getElementById('am_by_cat_btn').classList.add('active')
+
+    elements = document.getElementsByClassName("stat_elements_block");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.add('dnone');
+    }
+    document.getElementById('am_by_cat_block').classList.remove('dnone')
+
+
+    document.getElementById('chart').innerHTML = "";
+    document.getElementById('chart').classList.add("pie");
+    new Chartist.Pie('#chart', {
+        series: [20, 30, 50]
+    });
+
+}
+
+
+
+
+
+document.getElementById('cat_leader_btn').onclick = function () {
+    btns = document.getElementsByClassName("stat_nav_btn");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].classList.remove('active');
+    }
+    document.getElementById('cat_leader_btn').classList.add('active')
+
+    elements = document.getElementsByClassName("stat_elements_block");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.add('dnone');
+    }
+    document.getElementById('cat_leader_block').classList.remove('dnone')
+
+    document.getElementById('cat_leader_btn').classList.add('active')
+
+
+    document.getElementById('chart').innerHTML = "";
+    document.getElementById('chart').classList.remove("pie");
+    new Chartist.Bar('#chart', {
+        labels: ['Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec'],
+        series: [
+            [5, 4, 3, 7, 5, 10, 3, 5, 4, 3, 7, 5],
+        ]
+    }, {
+        axisX: {
+            showGrid: false,
+            offset: 30
+        },
+        axisY: {
+            showGrid: false,
+            offset: 30
+        }
+    });
+}
+
+document.getElementById('cat_dyn_btn').onclick = function () {
+    btns = document.getElementsByClassName("stat_nav_btn");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].classList.remove('active');
+    }
+    document.getElementById('cat_dyn_btn').classList.add('active')
+    document.getElementById('chart').innerHTML = "";
+    document.getElementById('chart').classList.remove("pie");
+
+    elements = document.getElementsByClassName("stat_elements_block");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.add('dnone');
+    }
+    document.getElementById('cat_dyn_block').classList.remove('dnone')
+
+    new Chartist.Line('#chart', {
+        labels: ['Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec', 'Dec'],
+        series: [
+            [5, 4, 3, 7, 5, 10, 3, 5, 4, 3, 7, 5],
+        ]
+    }, {
+        axisX: {
+            showGrid: false,
+            labelOffset: {
+                x: 0,
+                y: 10
+            },
+        },
+        axisY: {
+            showGrid: false,
+            labelOffset: {
+                x: -10,
+                y: 0
+            },
+        },
+        showPoint: false,
+        lineSmooth: false,
+    });
+}
+
+
+
+
+document.getElementById('cash_flow_btn').onclick = function () {
+    btns = document.getElementsByClassName("stat_nav_btn");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].classList.remove('active');
+    }
+    document.getElementById('cash_flow_btn').classList.add('active')
+    document.getElementById('chart').innerHTML = "";
+    document.getElementById('chart').classList.remove("pie");
+
+}
+
+
+
+
+
+
